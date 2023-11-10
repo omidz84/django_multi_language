84411835d4d119d01b4e763c18d4607b3fbed741
omid <omizare57@gmail.com>
2023-11-10 22:32:31 +0330

    Add custom middleware

--- django_multi_language/settings.py
+++ django_multi_language/settings.py
@@ -55,6 +55,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'user.middleware.LanguageMiddleware',
 ]
 
 ROOT_URLCONF = 'django_multi_language.urls'
